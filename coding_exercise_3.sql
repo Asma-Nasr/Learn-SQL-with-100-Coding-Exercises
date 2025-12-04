c6f58c037ac267fd5a2bbddbb73ea8939f1617c4
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2025-12-04 20:01:14 +0200

    Add SQL query for Sales department employees
    Add SQL query to select names of Sales employees with salary over $50,000.

---
+++ coding_exercise_3.sql
@@ -0,0 +1,8 @@
+/*
+Suppose you have a table named employees with the following columns: employee_id, name, department, and salary. 
+How would you write an SQL query to select the names of employees who work in the "Sales" department and have a salary greater than $50,000?
+*/
+SELECT name
+FROM employees
+WHERE department = 'Sales'
+    AND salary > 50000;
