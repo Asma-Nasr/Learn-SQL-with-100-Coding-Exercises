e6cbf25a42eb7c181bd293747d6fb45def8691a6
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2025-12-22 20:36:49 +0200

    Add SQL query for 1970 Nobel Prize winners
    Query to find Nobel Prize winners in 1970 excluding Physiology and Economics.

---
+++ coding_exercise_49.sql
@@ -0,0 +1,8 @@
+/*
+From the following table, write a SQL query to find the Nobel Prize winners in 1970 excluding the subjects of Physiology and Economics. 
+Return year, subject, winner, country, and category.
+*/
+SELECT *
+FROM nobel_win
+WHERE YEAR = 1970
+    AND SUBJECT NOT IN ('Physiology', 'Economics');
