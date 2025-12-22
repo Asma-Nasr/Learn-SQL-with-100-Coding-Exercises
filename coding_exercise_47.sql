cd0cad3a7bb371089ad4e2ef9a1e82088d161259
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2025-12-22 20:30:07 +0200

    Add SQL query to find winners named 'Louis'
    Retrieve details of winners with first name 'Louis'.

---
+++ coding_exercise_47.sql
@@ -0,0 +1,7 @@
+/*
+From the following table, write a SQL query to retrieve the details of the winners whose first names match with the string ‘Louis’. 
+Return year, subject, winner, country, and category.
+*/
+SELECT YEAR, SUBJECT, WINNER, COUNTRY, CATEGORY
+FROM nobel_win
+WHERE WINNER LIKE 'Louis %';
