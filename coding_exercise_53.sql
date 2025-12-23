c07c3b093a5f97b5f52a78e4ca3e1942cc5d93a1
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2025-12-23 18:54:07 +0200

    Create coding_exercise_53.sql

---
+++ coding_exercise_53.sql
@@ -0,0 +1,7 @@
+/*
+From the following table, write a SQL query to select a range of products whose price is in the range Rs.200 to Rs.600. 
+Begin and end values are included. Return pro_id, pro_name, pro_price, and pro_com.
+*/
+SELECT *
+FROM item_mast
+WHERE PRO_PRICE BETWEEN 200 AND 600;
