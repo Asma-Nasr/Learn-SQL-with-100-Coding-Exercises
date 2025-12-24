f7749f9cf1ca3ea44f02908a7c8f47cc4764c806
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2025-12-24 20:29:07 +0200

    Add SQL query to calculate average item price by company

---
+++ coding_exercise_57.sql
@@ -0,0 +1,6 @@
+/*
+From the following table, write a SQL query to calculate average price of the items for each company. Return average price and company code.
+*/
+SELECT PRO_COM AS company_code, AVG(PRO_PRICE) AS average_price
+FROM item_mast
+GROUP BY PRO_COM;
