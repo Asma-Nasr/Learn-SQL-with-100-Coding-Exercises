b1a4016471e69c5fc77eb899fdb99324e15b0c5c
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2026-01-01 20:08:32 +0200

    Add SQL query to retrieve commissions for Paris salespeople

---
+++ coding_exercise_67.sql
@@ -0,0 +1,7 @@
+/*
+From the following tables write a SQL query to determine the commission of the salespeople in Paris. 
+Return commission.
+*/
+SELECT commission
+FROM Salesman
+WHERE city = 'Paris';
