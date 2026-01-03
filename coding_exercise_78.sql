4808b11eeba29acc741e6bf67b034a68e5a7ce1d
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2026-01-03 20:48:32 +0200

    Create SQL query for single customer salespeople
    Add SQL query to find salespeople with a single customer.

---
+++ coding_exercise_78.sql
@@ -0,0 +1,6 @@
+/*
+From the following tables write a SQL query to find salespeople who deal with a single customer. Return salesman_id, name, city and commission.
+*/
+SELECT salesman_id, name, city, commission
+FROM Salesman
+WHERE salesman_id IN (SELECT c.salesman_id FROM Customer c GROUP BY c.salesman_id HAVING COUNT(*)=1);
